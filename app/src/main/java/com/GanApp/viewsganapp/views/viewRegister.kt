6d8956c283dd0a83e9fdeb346fe4c77c13107f5c
yuliguillen <yuliguillen2002@gmail.com>
2024-03-13 10:11:35 -0500

    Imagen del logo insertada, error de espacios solucionado y se incertaron iconos a los campos

--- app/src/main/java/com/GanApp/viewsganapp/views/viewRegister.kt
+++ app/src/main/java/com/GanApp/viewsganapp/views/viewRegister.kt
@@ -1,5 +1,6 @@
 package com.GanApp.viewsganapp.views
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -9,16 +10,26 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.GanApp.viewsganapp.R
 import androidx.compose.foundation.layout.Column as Column
 
 
@@ -29,50 +40,81 @@ fun UserInputForm(onSubmit: (UserData) -> Unit) {
     var password by remember { mutableStateOf("") }
     var numeroTelefono by remember { mutableStateOf("") }
 
+
     Column(
         modifier = Modifier
             .background(color = Color.White)
             .padding(16.dp)
             .fillMaxSize(), // Esto hará que la Column ocupe todo el tamaño disponible
-        horizontalAlignment = Alignment.CenterHorizontally, // Centrar los elementos horizontalmente
-        verticalArrangement = Arrangement.Center // Centrar los elementos verticalmente
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        
+
+        Row {
+            Image(painter = painterResource(id = R.drawable.logo), contentDescription ="Logo" )
+        }
+
         Text(text = "Registrate",
             fontSize = 40.sp,
             modifier = Modifier.padding(bottom = 16.dp)
         )
 
-        OutlinedTextField(
-            value = nombreCompleto,
-            onValueChange = { nombreCompleto = it },
-            label = { Text("Nombre") },
-            shape = RoundedCornerShape(30.dp) // Ajusta el radio del borde según tus preferencias
-        )
+        Box {
+            OutlinedTextField(
+                nombreCompleto,
+                { //Elimina las lineas no deseadas
+                    val filteredText = it.replace("\n", "")
+                    nombreCompleto = filteredText },
+                label = { Text("Nombre") },
+                textStyle = TextStyle(color = Color.Black),
+                leadingIcon = {
+                    Icon(imageVector = Icons.Default.Person, contentDescription = "telefono") },
+                shape = RoundedCornerShape(20.dp),// Ajusta el radio del borde según tus preferencias
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = Color.White,
+                    unfocusedContainerColor = Color.White,
+                    cursorColor = Color.Black )
+            )
+        }
 
 
         OutlinedTextField(
             value = correo,
-            onValueChange = { correo = it },
+            onValueChange = {
+                val filteredText = it.replace("\n", "")
+                correo = filteredText },
             label = { Text("Correo Electrónico") },
-            shape = RoundedCornerShape(30.dp)
+            textStyle = TextStyle(color = Color.Black),
+            leadingIcon = {
+                Icon(imageVector = Icons.Default.Email, contentDescription = "gmail") },
+            shape = RoundedCornerShape(20.dp)
         )
 
         OutlinedTextField(
             value = numeroTelefono,
-            onValueChange = { numeroTelefono = it },
+            onValueChange = {
+                val filteredText = it.replace("\n", "")
+                numeroTelefono = filteredText
+            },
             label = { Text("Número de Teléfono") },
-            shape = RoundedCornerShape(30.dp)
+            textStyle = TextStyle(color = Color.Black),
+            leadingIcon = {
+                Icon(imageVector = Icons.Default.Phone, contentDescription = "telefono")
+            },
+            shape = RoundedCornerShape(20.dp)
 
         )
 
         OutlinedTextField(
-            value = password,
-            onValueChange = { password = it },
+            password, { val filteredText = it.replace("\n", "")
+                password = filteredText },
             label = { Text("Contraseña") },
             visualTransformation = PasswordVisualTransformation(),
-            shape = RoundedCornerShape(30.dp)
-        )
+            textStyle = TextStyle(color = Color.Black),
+            leadingIcon = {
+                Icon(imageVector = Icons.Default.Lock, contentDescription = "telefono") },
+            shape = RoundedCornerShape(20.dp),
+            )
 
         Spacer(modifier = Modifier.height(16.dp)) // Añade espacio entre el formulario y el botón
 
