e5b7c8be45a7de0a25f527e7a29252094e909aed
nataly <nataly.pescador@gmail.com>
2024-03-27 19:14:15 -0500

    Agregando logica e interfaces para el proceso de recuperar contarseña

--- app/src/main/java/com/GanApp/viewsganapp/MainActivity.kt
+++ app/src/main/java/com/GanApp/viewsganapp/MainActivity.kt
@@ -6,8 +6,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.GanApp.viewsganapp.network.RetrofitInstance
 import com.GanApp.viewsganapp.ui.theme.ViewsGanAppTheme
+import com.GanApp.viewsganapp.views.ForgotPassword
 import com.GanApp.viewsganapp.views.LogIn
 import com.GanApp.viewsganapp.views.Register
+import com.GanApp.viewsganapp.views.ResetPassword
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -37,6 +39,40 @@ class MainActivity : ComponentActivity() {
 
                 LogIn { logInData ->
                     val call = RetrofitInstance.apiService.logIn(logInData)
+                    call.enqueue(object : Callback<Void> {
+                        override fun onResponse(call: Call<Void>, response: Response<Void>) {
+                            if (response.isSuccessful) {
+                                Log.d("API Call", "Success")
+                            } else {
+                                Log.d("API Call", "Response not successful: ${response.errorBody()?.string()}")
+                            }
+                        }
+
+                        override fun onFailure(call: Call<Void>, t: Throwable) {
+                            Log.d("API Call", "Failure: ${t.message}")
+                        }
+                    })
+                }
+
+                ForgotPassword { forgotPasswordData ->
+                    val call = RetrofitInstance.apiService.forgotPassword(forgotPasswordData)
+                    call.enqueue(object : Callback<Void> {
+                        override fun onResponse(call: Call<Void>, response: Response<Void>) {
+                            if (response.isSuccessful) {
+                                Log.d("API Call", "Success")
+                            } else {
+                                Log.d("API Call", "Response not successful: ${response.errorBody()?.string()}")
+                            }
+                        }
+
+                        override fun onFailure(call: Call<Void>, t: Throwable) {
+                            Log.d("API Call", "Failure: ${t.message}")
+                        }
+                    })
+                }
+
+                ResetPassword { resetPasswordData ->
+                    val call = RetrofitInstance.apiService.resetPassword(resetPasswordData)
                     call.enqueue(object : Callback<Void> {
                         override fun onResponse(call: Call<Void>, response: Response<Void>) {
                             if (response.isSuccessful) {
--- app/src/main/java/com/GanApp/viewsganapp/apiService/UserRegisterApiService.kt
+++ app/src/main/java/com/GanApp/viewsganapp/apiService/UserRegisterApiService.kt
@@ -1,7 +1,9 @@
 package com.GanApp.viewsganapp.apiService
 
 
+import com.GanApp.viewsganapp.views.ForgotPasswordData
 import com.GanApp.viewsganapp.views.LogInData
+import com.GanApp.viewsganapp.views.ResetPasswordData
 import com.GanApp.viewsganapp.views.UserData
 import retrofit2.Call
 import retrofit2.http.Body
@@ -11,6 +13,12 @@ interface UserRegisterApiService {
     @POST("registro")
     fun createUser(@Body userData: UserData): Call<Void>
 
-    @POST("iniciosesion")
+    @POST("inicio-sesion")
     fun logIn(@Body logInData: LogInData): Call<Void>
+
+    @POST("olvidar-contraseña")
+    fun forgotPassword(@Body forgotPasswordData: ForgotPasswordData): Call<Void>
+
+    @POST("restablecer-contraseña")
+    fun resetPassword(@Body resetPasswordData: ResetPasswordData): Call<Void>
 }
