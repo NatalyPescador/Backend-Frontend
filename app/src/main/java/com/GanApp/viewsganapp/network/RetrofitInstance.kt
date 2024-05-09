6d088878c8cbd346ce11fb8e79da405e467d06d5
nataly <nataly.pescador@gmail.com>
2024-05-09 16:21:27 -0500

    Cambios antes del pull con el menu hamburguesa

--- app/src/main/java/com/GanApp/viewsganapp/network/RetrofitInstance.kt
+++ app/src/main/java/com/GanApp/viewsganapp/network/RetrofitInstance.kt
@@ -9,7 +9,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
 
-    private const val BASE_URL = "http://192.168.0.102:8080/GanApp/" // Reemplaza esto con tu URL base
+    private const val BASE_URL = "http://192.168.1.13:8080/GanApp/" // Reemplaza esto con tu URL base
 
     // Lazy initialization del Retrofit instance
     private val retrofit: Retrofit by lazy {
--- app/src/main/java/com/GanApp/viewsganapp/views/productRegister.kt
+++ app/src/main/java/com/GanApp/viewsganapp/views/productRegister.kt
@@ -38,7 +38,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
