5e6f7846b8c3e309eb4e8474276024e0ef8d3dba
JohhanPC <johanlpc05@gmail.com>
2024-07-31 08:50:13 -0500

    cambio url

--- app/src/main/java/com/GanApp/viewsganapp/utils/BaseUrlConstant.kt
+++ app/src/main/java/com/GanApp/viewsganapp/utils/BaseUrlConstant.kt
@@ -1,5 +1,5 @@
 package com.GanApp.viewsganapp.utils
 
 object BaseUrlConstant {
-    const val BASE_URL = "http://10.175.145.97:8080/GanApp/"
+    const val BASE_URL = "http://192.168.10.8:8080/GanApp/"
 }
