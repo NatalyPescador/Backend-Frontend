1e1e015a0a50b6020f3ef142c758b34b2719bc06
JohhanPC <johanlpc05@gmail.com>
2024-09-11 19:24:29 -0500

    Cambios

--- app/src/main/java/com/GanApp/viewsganapp/utils/BaseUrlConstant.kt
+++ app/src/main/java/com/GanApp/viewsganapp/utils/BaseUrlConstant.kt
@@ -1,5 +1,5 @@
 package com.GanApp.viewsganapp.utils
 
 object BaseUrlConstant {
-    const val BASE_URL = "http://10.175.144.123:8080/GanApp/"
+    const val BASE_URL = "https://gannap-production.up.railway.app/GanApp/"
 }
--- app/src/main/java/com/GanApp/viewsganapp/views/editProfile.kt
+++ app/src/main/java/com/GanApp/viewsganapp/views/editProfile.kt
@@ -33,7 +33,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import coil.compose.rememberAsyncImagePainter
 import com.GanApp.viewsganapp.models.UserDto
-import com.GanApp.viewsganapp.viewmodels.UserProfileViewModel
+import com.GanApp.viewsganapp.viewModels.UserProfileViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
